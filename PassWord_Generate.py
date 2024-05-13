289fb5ffc0c9a06cc2f82637ec93752454d4b60f
Tohar <104694378+ASDASASAssss@users.noreply.github.com>
2024-05-13 15:55:21 +0300

    Add files via upload

---
+++ PassWord_Generate.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = int(input("Enter the length of the password: "))
+    password = generate_password(password_length)
+    print("PassWord Ready, do not share ur pass to anyone!:", password)
+    print ("@Made By Tohar")
